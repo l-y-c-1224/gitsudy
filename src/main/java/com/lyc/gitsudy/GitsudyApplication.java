3a64f1345f0b0a8ca7eb2a6fe8707a354be13e3a
l-y-c-1224 <17610750093@163.com>
2020-11-23 14:59:46 +0800

    modify one demo

---
+++ src/main/java/com/lyc/gitsudy/GitsudyApplication.java
@@ -0,0 +1,13 @@
+package com.lyc.gitsudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitsudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitsudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lyc/gitsudy/GitsudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyc.gitsudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitsudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
